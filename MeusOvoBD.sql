1c7f87b525dc995c7c68d6ad844df0ec684583d4
The LEGEND <87276808+gguisales@users.noreply.github.com>
2022-11-14 21:55:59 -0300

    uptadebdquasepronto

---
+++ MeusOvoBD.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE meusovo;
+USE meusovo;
+
+CREATE TABLE produto(
+	cd INT PRIMARY KEY NOT NULL,
+    nome VARCHAR (20),
+    descrição VARCHAR (90),
+    preço INT,
+    genero CHAR (20),
+    status_prod VARCHAR (20)
+);
+CREATE TABLE pedido(
+	cd_pedido INT PRIMARY KEY NOT NULL,
+	nome_cliente VARCHAR (30),
+	email_cliente VARCHAR (30),
+	telefone_cliente BIGINT (14),
+	endereço VARCHAR (100),
+	cep VARCHAR (10),
+	status_pedido VARCHAR (10)
+);
+CREATE TABLE fatura(
+cd_fatura INT PRIMARY KEY NOT NULL,
+preço_unit INT,
+data_fatura DATE,
+VL_TOTAL INT,
+cd_produto INT,
+ foreign key (cd_produto) REFERENCES produto (cd)
+);
+
+
+
+
+
+
+
+
+
